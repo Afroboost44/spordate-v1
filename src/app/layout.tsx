921e3dfda90a13bf7a0e62fa5b025293d5119511
Afroboost44 <contact.artboost@gmail.com>
2026-03-20 14:10:55 +0100

    Add PWARegister component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { AuthProvider } from "@/context/AuthContext";
+import PWARegister from "@/components/PWARegister";
 
 export const metadata: Metadata = {
   title: "Spordate",
@@ -42,6 +43,7 @@ export default function RootLayout({
           <LanguageProvider>
             {children}
             <Toaster />
+            <PWARegister />
           </LanguageProvider>
         </AuthProvider>
       </body>
